98fdb3558a877715c3271ab98dd8907431f12770
Stefan Gasser <stefan.gasser@additive.eu>
2020-03-29 00:03:02 +0100

    Add NewEvents + Banner + fix autoplay

--- src/components/EmbeddedVideo.vue
+++ src/components/EmbeddedVideo.vue
@@ -3,7 +3,7 @@
     <iframe
       frameborder="0"
       height="100%"
-      v-bind:src="'https://www.facebook.com/v2.3/plugins/video.php?allowfullscreen=true&autoplay=true&container_width=800&href='+videoUrl"
+      v-bind:src="'https://www.facebook.com/v2.3/plugins/video.php?allowfullscreen=true&autoplay=false&container_width=800&href='+videoUrl"
       width="100%"/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,25 @@
   <div>
     <NavMenu/>
     <Header/>
-    <Footer/>
+    <NewEvents/>
+    <Banner/>
+    <Footer class="pb-10" />
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
   import NavMenu from "../components/NavMenu";
-  import Header from '@/components/Header.vue'
+  import Header from '../components/Header.vue'
+  import NewEvents from "../components/NewEvents";
   import Footer from "../components/Footer";
+  import Banner from "../components/Banner";
 
   export default {
     name: 'Home',
     components: {
+      Banner,
+      NewEvents,
       Footer,
       NavMenu,
       Header
