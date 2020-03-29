6ecf5b411530979c6d1ad810d62f7b4c0d3680e9
Stefan Gasser <stefan.gasser@additive.eu>
2020-03-29 01:14:14 +0100

    Update text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6">
       <div class="text-center">
         <h2
-          class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+          class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl pb-6">
           🤔 Wos geat
           <br class="xl:hidden"/>
           <span class="text-indigo-600">online?</span>
@@ -11,6 +11,8 @@
         <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           WosGeat.Online hilft dir den passenden Livestream aus Südtirol zu finden.
           Egal ob Kochen, Sport oder Musik, es ist für jeden was dabei.
+        </p>
+        <p class="mt-3 max-w-md mx-auto text-base text-gray-700 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           #ibleibdohoam ober bin net aloan
         </p>
         <form class="mt-16 max-w-md mx-auto sm:flex sm:justify-center md:mt-24" v-on:submit.prevent="search">
