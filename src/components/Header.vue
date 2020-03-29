52ef9ac99c55f467c3057e5786658c2709c72775
Stefan Gasser <stefan.gasser@additive.eu>
2020-03-29 01:15:02 +0100

    Remove shadow from button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,7 +32,7 @@
                 v-model="query"/>
             </div>
           </div>
-          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+          <div class="mt-3 rounded-md sm:mt-0 sm:ml-3">
             <button
               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
               v-on:click="search">
